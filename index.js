280971902fd0deefa2d09cef92690026e7588e4e
Giotje_ <devgio@icloud.com>
2021-08-20 16:23:00 +0200

    added reeact from import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Plugin } = require('powercord/entities');
-const { getModule } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
 
 module.exports = class PowerSave extends Plugin {
